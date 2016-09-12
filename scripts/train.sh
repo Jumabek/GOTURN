7f143319077255c618a9aeb098664fac96fd24ba
Jumabek <jumabek4044@gmail.com>
2016-09-12 18:51:36 +0900

    change in nets

--- scripts/train.sh
+++ scripts/train.sh
@@ -7,8 +7,8 @@ if [ -z "$4" ]
     exit
 fi
 
-GPU_ID=0
-MODEL_DIR=nets/models/pr_tr_smlr_scrtch_npd
+GPU_ID=1
+MODEL_DIR=nets/models/pr_tr_smlr_npd
 FOLDER=GOTURN1
 RANDOM_SEED=800
 
@@ -25,7 +25,7 @@ SOLVER=$MODEL_DIR/solver.prototxt
 TRAIN_PROTO=$MODEL_DIR/tracker.prototxt
 #CAFFE_MODEL=nets/models/weights_init/tracker_init.caffemodel
 #CAFFE_MODEL=NONE
-CAFFE_MODEL=../caffe/models/bvlc_reference_caffenet_npd/caffenet_train_iter_450000.caffemodel
+CAFFE_MODEL=../caffe/models/bvlc_reference_caffenet_npd/caffenet_train_iter_500000.caffemodel
 
 
 
